ba26b01930c5436b4a82fb650171ab93e6924508
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2018-06-05 18:32:42 +0530

    Bug Fixes and some minor changes

--- R/bd_synonyms.R
+++ R/bd_synonyms.R
@@ -5,25 +5,42 @@
 #' @param name (string) scientfic name of the species.
 #' @return returns a data.frame which contains synonyms and other related taxonomic info.
 #' @example 
-#' df<-bd_synonym("Chironomus riparius") #Enter the data base from the choices, data base supported are 'itis','col','eol','tropicos','nbn','worms'
+#' df<-bd_synonym("Chironomus riparius") #Enter the data base from the choices, data base supported are 'itis','col','eol','tropicos','nbn'
 #' 
+#' df<-bd_synonym("Balaenoptera musculus") #Marine species example "worms data base"
+#' 
+#' df<-bd_synonym("Gadus morhua") #With marine i.e. worms database this will return synonym and with other databases it will return error
+#' 
+#' df<-bd_synonym("Aquila chrysaetos") #"nbn" data base example
 #'
-#'
-#'
+#' df<-bd_synonym("Puma concolor") #"itis", "col" database example
 
 # ----------Function Start---------- 
 
 bd_synonym<-function(name=NULL){
   
-  db1<- readline(prompt="Enter The data base from where you want synonym, supported data base are 'itis','col','eol','tropicos','nbn','worms' ")
-  if(db1=="itis"|| db1=="col" || db1=="eol" || db1=="tropicos" || db1=="worms" || db1=="nbn"){
+  db1<- readline(prompt="Enter The data base from where you want synonym, 
+                 below are the choices (for marine species use worms as the data base)
+                 1) 'itis'
+                 2) 'col'
+                 3) 'tropicos'
+                 4) 'nbn'
+                 5) 'worms' ")
+  if(db1=="itis"|| db1=="col" || db1=="tropicos" || db1=="nbn"){
     
     id<-taxize::get_ids(names=name, db=db1)
     attr<-lapply(id, attributes)
     if((attr[[1]]$match)=="found"){
       syn<-taxize::synonyms(id,db=db1)
-      return(as.data.frame(syn[[1]][[1]]))
-      
+      df<-as.data.frame(syn[[1]][[1]])
+      if(nrow(df)==0){
+        
+        stop("No match for synonym with the data base, please change the data base selected")
+        
+      }else{
+        
+        return(df)
+      }
       
     }else{
       
@@ -31,9 +48,23 @@ bd_synonym<-function(name=NULL){
       
     }
     
+  }else if(db1=="worms"){
+    
+    syn<-synonyms(name,db="worms")
+    df<-as.data.frame(syn[[1]])
+    if(nrow(df)==0){
+      
+      stop("There is no match for synonym with worms data base")
+      
+    }else{
+      
+      return(df)
+      
+    }
+    
   }else{
     
-    stop("The entered data base is incorrect, please select from 'itis','col','eol','tropicos','nbn','worms' ")
+    stop("The entered data base is incorrect, please select from 'itis','col','tropicos','nbn','worms' ")
   }
   
   
