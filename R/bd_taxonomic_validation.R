abfce0f1e23cc9c414afee7f82fbe6c1a0ad606b
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2018-06-19 16:43:02 +0530

    Function for Taxonomic Validation

---
+++ R/bd_taxonomic_validation.R
@@ -0,0 +1,58 @@
+#'
+#'
+#' @title Function for taxonomic validation
+#'
+#' @name bd_taxonomic_validation
+#' @param df (data.frame) contains a data frame with species data
+#' @example 
+#' key <- name_backbone(name='Puma concolor')$speciesKey
+#' dat <- occ_search(taxonKey=key, return='data', limit=1200)
+#' bd_taxonomic_validation(dat) #Enter the taxonomic level and value, kingdom-Animalia 
+#' 
+#' key <- name_backbone(name='Trochilidae')$speciesKey
+#' dat <- occ_search(taxonKey=key, return='data', limit=1200)
+#' bd_taxonomic_validation(dat) #Enter taxonomic level and value, order-Carnivora
+#' 
+#' 
+#' key <- name_backbone(name='Delphinids')$speciesKey
+#' dat <- occ_search(taxonKey=key, return='data', limit=1200)
+#' bd_taxonomic_validation(dat) #Enter taxonomic level and value, phylum-chordata
+#' 
+
+
+#---------- Function Start----------
+
+
+bd_taxonomic_validation<-function(df=NULL){
+  
+  
+  tax_lvl<- readline(prompt="Choose the taxonomic level for validating the data
+                 1) kingdom
+                 2) phylum
+                 3) class
+                 4) order
+                 5) family
+                 6) genus ")
+  
+  tax_ch<-readline(prompt="Enter the value of taxonomic level chosen above")
+  
+  if(tax_lvl %in% names(df)){
+    
+    df1<-apply(df,2,tolower)
+    out<-as.integer(df1[,tax_lvl]==tolower(tax_ch))
+    out[is.na(out)]<-0
+    per<-(sum(out)/nrow(dat))*100
+    mis<-as.double(nrow(df)-sum(out))
+    
+    cat("The number of records with mismatch with given taxonomic level and value are:-",mis,"\n")
+    cat("The percentage of matched records with given taxonomic level and value are :- ",per,"\n")
+    
+  }else{
+    
+    stop("The entered taxonomic level is not present in data")
+    
+  }
+  
+}
+
+#----------Function End----------
